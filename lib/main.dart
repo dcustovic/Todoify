ba94a31bd9560017803c68d7dad3208a26c593de
dcustovic <domcus44@gmail.com>
2022-10-02 17:39:34 +0200

    formatting routes #2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_flutter/constants/routes.dart';
 import 'package:notes_flutter/views/notes_view.dart';
 
 import 'firebase_options.dart';
@@ -18,9 +19,9 @@ void main() {
       ),
       home: const HomePage(),
       routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-        '/notes/': (context) => const NotesView(),
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
       }));
 }
 
