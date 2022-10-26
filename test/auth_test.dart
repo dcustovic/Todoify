be5a183b513dcfb7dd1a040f1e43f081ecdac4dd
dcustovic <domcus44@gmail.com>
2022-10-26 12:49:22 +0200

    fixing tests

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -110,7 +110,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'wrong@email.com') throw UserNotFoundAuthException();
     if (password == "wrongpassword") throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false);
+    const user = AuthUser(isEmailVerified: false, email: 'wrong@email.com');
     _user = user;
     return Future.value(user);
   }
@@ -128,7 +128,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true);
+    const newUser = AuthUser(isEmailVerified: true, email: 'wrong@email.com');
     _user = newUser;
   }
 }
