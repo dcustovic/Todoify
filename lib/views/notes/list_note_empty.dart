7011e0956785b82788fa675088f936b058e005b5
dcustovic <domcus44@gmail.com>
2022-11-23 14:45:49 +0100

    version

--- lib/views/notes/list_note_empty.dart
+++ lib/views/notes/list_note_empty.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 class ListNoteEmpty extends StatelessWidget {
@@ -7,39 +8,42 @@ class ListNoteEmpty extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 95, 81, 223),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 60),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/list-empty-image.png',
-                width: 230,
-              ),
-              const Text(
-                "Oh no!",
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Color.fromARGB(122, 255, 255, 255),
+      body: FadeIn(
+        duration: const Duration(seconds: 1),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 60),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/list-empty-image.png',
+                  width: 230,
                 ),
-              ),
-              const Text(
-                "It appears your to-do list is empty",
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Color.fromARGB(122, 255, 255, 255),
+                const Text(
+                  "Oh no!",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Color.fromARGB(122, 255, 255, 255),
+                  ),
                 ),
-              ),
-              const Text(
-                "Swipe left to create one",
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Color.fromARGB(122, 255, 255, 255),
+                const Text(
+                  "It appears your to-do list is empty",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Color.fromARGB(122, 255, 255, 255),
+                  ),
                 ),
-              )
-            ],
+                const Text(
+                  "Swipe left to create one",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Color.fromARGB(122, 255, 255, 255),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
